deff6a65e95c02ab69e83d016004e698d7f8308d
Cristian Gonzales <xcristian.gonzales@gmail.com>
2017-07-21 23:43:12 -0700

    Minor changes to Maps

--- stay-safe/stay-safe/MapViewController.swift
+++ stay-safe/stay-safe/MapViewController.swift
@@ -17,19 +17,15 @@ class MapViewController: UIViewController {
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-//        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        let rect = CGRect(origin: CGPoint(x: 0,y :0), size: CGSize(width: 100, height: 100))
-//        let mapView = GMSMapView.map(withFrame: CGRectMake(0, 0, 100, 100), camera: camera)
-        let mapView = GMSMapView.map(withFrame: rect, camera: camera)
-
+//        let rect = CGRect(origin: CGPoint(x: 0,y :0), size: CGSize(width: 100, height: 100))
+//        let mapView = GMSMapView.map(withFrame: rect, camera: camera)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         mapView.isMyLocationEnabled = true
-        self.view = mapView
+        self.view.addSubview(mapView)
         
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
         marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-//        marker.title = "Sydney"
-//        marker.snippet = "Australia"
         marker.map = mapView
         
     }
